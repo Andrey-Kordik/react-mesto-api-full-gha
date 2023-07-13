5fd2fb2d5e0f1a6d36a43d6492800480ebc60fe8
Andrey Kordik <andreykordik@yandex.ru>
2023-07-13 21:06:15 +0400

    15 sprint

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const validator = require('validator');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const regex = /(https?:\/\/)(www)?([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=])*#?$/;
 
@@ -39,7 +40,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8,
     select: false,
   },
 }, { versionKey: false });
@@ -48,15 +48,10 @@ userSchema.statics.findUserByCredentials = function signIn(email, password) {
   return this.findOne({ email }, { runValidators: true })
     .select('+password')
     .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
-
+      if (!user) throw new UnauthorizedError('Неправильные почта или пароль');
       return bcrypt.compare(password, user.password)
         .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
-          }
+          if (!matched) throw new UnauthorizedError('Неправильные почта или пароль');
 
           return user;
         });
