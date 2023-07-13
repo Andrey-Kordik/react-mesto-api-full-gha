9510d8f85ff24579c681da0cf3d328363e22aaf0
Andrey Kordik <andreykordik@yandex.ru>
2023-07-13 21:50:51 +0400

    15 sprint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,15 +4,15 @@ const {
   getUsers, getUserById, updateUserData, updateUserAvatar, getMyData, logout,
 } = require('../controllers/users');
 
-router.get('//me', getMyData);
+router.get('/me', getMyData);
 
-router.patch('//me', updateUserData);
+router.patch('/me', updateUserData);
 
-router.patch('//me/avatar', updateUserAvatar);
+router.patch('/me/avatar', updateUserAvatar);
 
 router.get('/', getUsers);
 
-router.get('//:userId', getUserById);
+router.get('/:userId', getUserById);
 
 router.get('/signout', logout);
 
